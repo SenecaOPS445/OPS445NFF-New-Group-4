d93cb52f9a3f4e51b161245defbf4b2edfa3369e
ram sablok <rjsablok@myseneca.ca>
2025-04-01 17:37:16 -0400

    main function

--- main.py
+++ main.py
@@ -2,6 +2,4 @@
 if __name__ == "__main__":
     # Set up argument parser
     parser = argparse.ArgumentParser()
-    # Define the argument for resource (cpu or ram)
-    parser.add_argument('resource', help="Specify 'cpu' for CPU usage or 'ram' for RAM usage.", type=str, default=None)
-
+    
