462848a4b401ba1f866bc14f2de7d3ef63733eb5
- Rohit <rohit7@myseneca.ca>
2025-04-03 18:59:34 -0400

    commit2

--- ramfunc.py
+++ ramfunc.py
@@ -3,6 +3,12 @@ def get_ram_usage():
     lines = f.readlines()
     f.close()
 
-    # Extract total and free memory values
-    total_memory = int(lines[0].split()[1])  
+    # To extract the total ram and free memory values 
+    total_memory = int(lines[0].split()[1])   
     free_memory = int(lines[1].split()[1])   
+    buffers = int(lines[3].split()[1])
+    cached = int(lines[4].split()[1])
+
+
+
+
